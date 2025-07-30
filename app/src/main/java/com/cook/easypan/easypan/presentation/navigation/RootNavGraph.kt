1d9a339ee4c1a76a0d2b54dbc8d57446a8a51256
Dmytro <dim4ukt@gmail.com>
2025-07-30 13:52:36 +0200

    bug fix

--- app/src/main/java/com/cook/easypan/easypan/presentation/navigation/RootNavGraph.kt
+++ app/src/main/java/com/cook/easypan/easypan/presentation/navigation/RootNavGraph.kt
@@ -10,32 +10,32 @@ import org.koin.java.KoinJavaComponent.inject
 
 @Composable
 fun RootNavGraph() {
-        val navController = rememberNavController()
+    val navController = rememberNavController()
     val userRepository: UserRepository by inject(UserRepository::class.java)
-        NavHost(
-            navController = navController,
-            startDestination = if (userRepository.isUserSignedIn()) {
-                Route.AppGraph
-            } else {
-                Route.AuthGraph
-            }
-        ) {
-            authNavGraph(
-                navController = navController,
-            )
-            composable<Route.AppGraph> {
-                Home(
-                    onSignOut = {
-                        navController.navigate(Route.AuthGraph) {
-                            popUpTo(Route.AppGraph) {
-                                inclusive = true
-                            }
-                        }
-                        userRepository.signOut()
-                    }
-                )
-            }
+    NavHost(
+        navController = navController,
+        startDestination = if (userRepository.isUserSignedIn()) {
+            Route.AppGraph
+        } else {
+            Route.AuthGraph
         }
+    ) {
+        authNavGraph(
+            navController = navController,
+            )
+        composable<Route.AppGraph> {
+            Home(
+                onSignOut = {
+                    navController.navigate(Route.AuthGraph) {
+                        popUpTo(Route.AppGraph) {
+                            inclusive = true
+                        }
+                    }
+                    userRepository.signOut()
+                }
+            )
+        }
+    }
 
 }
 
